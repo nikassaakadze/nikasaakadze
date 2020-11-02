9dd2578dc3e55ec72dd95a321f226a39e47e8df1
nikassaakadze <nikasaakade2@gmail.com>
2020-11-02 09:06:43 -0800

    portfolio

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function openNav(){
+    var nav = document.querySelector(".responsive-nav")
+    nav.style.height = "200px"
+    nav.style.visibility = "visible"
+}
+
+function closeNav(){
+  var nav = document.querySelector(".responsive-nav")
+  nav.style.height = "0px"
+  nav.style.visibility = "hidden"
+}
